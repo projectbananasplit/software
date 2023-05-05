4a9955dc0e9be5fe738ad873a5b005e79a265fc8
Rotzbua <Rotzbua@users.noreply.github.com>
2023-05-05 18:11:18 +0200

    Update user_install.bat

--- user_install.bat
+++ user_install.bat
@@ -7,3 +7,7 @@ winget install %param% --id "9PP3C07GTVRH"
 winget install %param% --id "XPDM27W10192Q0"
 : InkScape
 winget install %param% --id "9PD9BHGLFC7H"
+: Visual Studio Code
+winget install %param% --id "XP9KHM4BK9FZ7Q"
+: Monitorian
+winget install %param% --id "9NW33J738BL0"
