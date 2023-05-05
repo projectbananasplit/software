2a88eaa0863d732b1f8ae6216ac6628b294d5868
Rotzbua <Rotzbua@users.noreply.github.com>
2023-05-05 18:04:08 +0200

    Update user_install.bat

--- user_install.bat
+++ user_install.bat
@@ -1,7 +1,9 @@
 @echo off
-set param=--accept-source-agreements --silent
+set param=--accept-source-agreements --accept-package-agreements --silent
 
-# Gimp
+: Blender
+winget install %param% --id "9PP3C07GTVRH"
+: Gimp
 winget install %param% --id "XPDM27W10192Q0"
-# InkScape
+: InkScape
 winget install %param% --id "9PD9BHGLFC7H"
