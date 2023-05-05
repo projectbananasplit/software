735d4663b89d8a3b1f4dbcb25f28f598a8408f50
Rotzbua <Rotzbua@users.noreply.github.com>
2023-05-05 19:14:50 +0200

    Update admin_install.bat

--- admin_install.bat
+++ admin_install.bat
@@ -1,6 +1,18 @@
 @echo off
 set param=--accept-source-agreements --accept-package-agreements --silent
 
+: MSSTORE =======================
 : VLC
 winget install %param% --id "XPDM1ZW6815MQM"
 
+: winget ========================
+: PrusaSlicer
+winget install %param% --id "Prusa3D.PrusaSlicer"
+: Meshlab
+winget install %param% --id "CNRISTI.MeshLab"
+: OpenSCAD
+winget install %param% --id "OpenSCAD.OpenSCAD"
+: LibreOffice
+winget install %param% --id "TheDocumentFoundation.LibreOffice"
+: Cura
+winget install %param% --id "Ultimaker.Cura"
