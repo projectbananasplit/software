78c214e174d06c230f23a3ac7ddc7cfd8a1d522f
Rotzbua <Rotzbua@users.noreply.github.com>
2023-05-11 17:50:49 +0200

    Update admin_clean.bat

--- admin_clean.bat
+++ admin_clean.bat
@@ -15,29 +15,29 @@ echo %blue%##############################
 echo %blue%winget
 echo %blue%##############################
 echo %green%7zip
-winget uninstall %param% --id "7zip.7zip"
+winget uninstall %param% -e --id "7zip.7zip"
 echo %green%paint.net
-winget uninstall %param% --id "dotPDNLLC.paintdotnet"
+winget uninstall %param% -e --id "dotPDNLLC.paintdotnet"
 echo %green%OpenOffice
-winget uninstall %param% --id "Apache.OpenOffice"
+winget uninstall %param% -e --id "Apache.OpenOffice"
 echo %green%Inkscape
-winget uninstall %param% --id "Inkscape.Inkscape"
+winget uninstall %param% -e --id "Inkscape.Inkscape"
 echo %green%Gimp
-winget uninstall %param% --id "Gimp.Gimp"
+winget uninstall %param% -e --id "Gimp.Gimp"
 echo %green%Google Chrome
-winget uninstall %param% --id "Google.Chrome"
+winget uninstall %param% -e --id "Google.Chrome"
 echo %green%x263vfw
-winget uninstall %param% --id "x263vfw64"
+winget uninstall %param% -e --id "x263vfw64"
 echo %green%Maxon Cinema 4D R25
-winget uninstall %param% --id "Maxon Cinema 4D R25"
+winget uninstall %param% -e --id "Maxon Cinema 4D R25"
 echo %green%Magic Bullet Suite
 winget uninstall %param% --name "Magic Bullet Suite"
 
 echo %green%Python.Python.3.6
-winget install %param% --id "Python.Python.3.6"
+winget install %param% -e --id "Python.Python.3.6"
 echo %green%Python.Python.3.7
-winget install %param% --id "Python.Python.3.7"
+winget install %param% -e --id "Python.Python.3.7"
 echo %green%Python.Python.3.8
-winget install %param% --id "Python.Python.3.8"
+winget install %param% -e --id "Python.Python.3.8"
 echo %green%Python.Python.3.9
-winget install %param% --id "Python.Python.3.9"
+winget install %param% -e --id "Python.Python.3.9"
