2cd70386b1b4d10a7b30baf70018276e56983cc6
Rotzbua <Rotzbua@users.noreply.github.com>
2023-05-11 17:52:00 +0200

    Update user_install.bat

--- user_install.bat
+++ user_install.bat
@@ -37,32 +37,32 @@ echo %purp%==============================
 echo %purp%Windows Erweiterungen
 echo %purp%==============================
 echo %green%Webp-Bilderweiterungen
-winget install %param% --id "9PG2DK419DRG"
+winget install %param% -e --id "9PG2DK419DRG"
 echo %green%Raw-Bilderweiterung
-winget install %param% --id "9NCTDW2W1BH8"
+winget install %param% -e --id "9NCTDW2W1BH8"
 echo %green%HEIF-Bilderweiterungen
-winget install %param% --id "9PMMSR1CGPWG"
+winget install %param% -e --id "9PMMSR1CGPWG"
 echo %green%AV1 Video Extension
-winget install %param% --id "9MVZQVXJBQ9V"
+winget install %param% -e --id "9MVZQVXJBQ9V"
 echo %green%VP9 Video Erweiterung
-winget install %param% --id "9N4D0MSMP0PT"
+winget install %param% -e --id "9N4D0MSMP0PT"
 echo %purp%==============================
 echo %purp%Anwendungen
 echo %purp%==============================
 echo %green%Blender
-winget install %param% --id "9PP3C07GTVRH"
+winget install %param% -e --id "9PP3C07GTVRH"
 echo %green%Gimp
-winget install %param% --id "XPDM27W10192Q0"
+winget install %param% -e --id "XPDM27W10192Q0"
 echo %green%InkScape
-winget install %param% --id "9PD9BHGLFC7H"
+winget install %param% -e --id "9PD9BHGLFC7H"
 echo %green%Visual Studio Code
-winget install %param% --id "XP9KHM4BK9FZ7Q"
+winget install %param% -e --id "XP9KHM4BK9FZ7Q"
 echo %green%Monitorian
-winget install %param% --id "9NW33J738BL0"
+winget install %param% -e --id "9NW33J738BL0"
 echo %green%NanaZip
-winget install %param% --id "9N8G7TSCL18R"
+winget install %param% -e --id "9N8G7TSCL18R"
 echo %green%Cinebench
-winget install %param% --id "9PGZKJC81Q7J"
+winget install %param% -e --id "9PGZKJC81Q7J"
 
 
 : winget ========================
@@ -70,5 +70,5 @@ winget install %param% --id "9PGZKJC81Q7J"
 : echo %blue%winget
 : echo %blue%##############################
 : echo %green%Figma
-:winget install %param% --id "Figma.Figma"
+:winget install %param% -e --id "Figma.Figma"
 
