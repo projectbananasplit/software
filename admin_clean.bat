ee9142e99082974d14e518a7647c5a5c330f231b
Rotzbua <Rotzbua@users.noreply.github.com>
2023-05-11 12:49:49 +0200

    f

--- admin_clean.bat
+++ admin_clean.bat
@@ -1,5 +1,24 @@
 @echo off
 set param=--accept-source-agreements --accept-package-agreements --silent
 
-: 7zip
+@for /f %%a in ('echo prompt $E^| cmd') do set "esc=%%a"
+set red=%esc%[38;5;1m
+set green=%esc%[38;5;2m
+set yellow=%esc%[38;5;11m
+set blue=%esc%[38;5;12m
+set purp=%esc%[38;5;13m
+set white=%esc%[38;5;15m
+
+echo %blue%##############################
+echo %blue%REMOVE if possible
+echo %blue%##############################
+echo %blue%winget
+echo %blue%##############################
+echo %green%7zip
 winget uninstall %param% --id "7zip.7zip"
+echo %green%paint.net
+winget uninstall %param% --id "dotPDNLLC.paintdotnet"
+echo %green%OpenOffice
+winget uninstall %param% --id "Apache.OpenOffice"
+echo %green%Inkscape
+winget uninstall %param% --id "Inkscape.Inkscape"
--- admin_install.bat
+++ admin_install.bat
@@ -1,35 +1,64 @@
 @echo off
 set param=--accept-source-agreements --accept-package-agreements --silent
 
-: MSSTORE =======================
+@for /f %%a in ('echo prompt $E^| cmd') do set "esc=%%a"
+set red=%esc%[38;5;1m
+set green=%esc%[38;5;2m
+set yellow=%esc%[38;5;11m
+set blue=%esc%[38;5;12m
+set purp=%esc%[38;5;13m
+set white=%esc%[38;5;15m
+
+echo %yellow%Senf this PC.
+echo %yellow%
+echo %yellow%"    ___________
+echo %yellow%"   [___________]
+echo %yellow%"   /           \\
+echo %yellow%"  /~~^~^~^~^~^~^\\
+echo %yellow%" /===============\\
+echo %yellow%";  _   _       ___ ;
+echo %yellow%"| |_  |_ |\\ | |_  |
+echo %yellow%";  _| |_ | \\| |   ;
+echo %yellow%" \\===============/
+echo %yellow%"  \\             /
+echo %yellow%"   `"""""""""""`
+
+echo %blue%##############################
+echo %blue%MSSTORE
+echo %blue%##############################
 : VLC nicht up to date
 : winget install %param% --id "XPDM1ZW6815MQM"
 
-: winget ========================
-: Git
+echo %blue%##############################
+echo %blue%winget
+echo %blue%##############################
+echo %green%Git
 winget install %param% --id "Git.Git"
-: VLC
+echo %green%Python.Python.3.10
+winget install %param% --id "Python.Python.3.10"
+echo %green%VLC
 winget install %param% --id "VideoLAN.VLC"
-: PrusaSlicer
+echo %green%PrusaSlicer
 winget install %param% --id "Prusa3D.PrusaSlicer"
-: Meshlab
+echo %green%Meshlab
 winget install %param% --id "CNRISTI.MeshLab"
-: OpenSCAD
+echo %green%OpenSCAD
 winget install %param% --id "OpenSCAD.OpenSCAD"
-: LibreOffice
+echo %green%LibreOffice
 winget install %param% --id "TheDocumentFoundation.LibreOffice"
-: Cura
+echo %green%Cura
 winget install %param% --id "Ultimaker.Cura"
-: KiCad
+echo %green%KiCad
 winget install %param% --id "KiCad.KiCad"
-: Notepad++
+echo %green%Notepad++
 winget install %param% --id "Notepad++.Notepad++"
-: FreeCAD
+echo %green%FreeCAD
 winget install %param% --id "FreeCAD.FreeCAD"
 
-: Python.Python.3.10
-winget install %param% --id "Python.Python.3.10"
 
-: Microsoft Visual C++ 2015-20XX Redistributable
+echo %purp%==============================
+echo %purp%Windows Erweiterungen
+echo %purp%==============================
+echo %green%Microsoft Visual C++ 2015-20XX Redistributable 32/64bit
 winget install %param% --id "Microsoft.VCRedist.2015+.x64"
 winget install %param% --id "Microsoft.VCRedist.2015+.x86"
