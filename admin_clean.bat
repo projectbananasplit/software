b04facb441bc2d43bdaf778e6f1a631d760e7916
Rotzbua <Rotzbua@users.noreply.github.com>
2023-07-06 12:27:45 +0200

    Update admin_clean.bat

--- admin_clean.bat
+++ admin_clean.bat
@@ -38,7 +38,12 @@ echo %green%Ultimaker Cura
 winget uninstall %param% --name "Ultimaker Cura 4.13.0"
 
 echo %green%Arduino
-winget uninstall %param% -e --id "Arduino"
+winget uninstall %param% --purge -e --id "Arduino"
+
+echo %green%FARO LS 1.1.800.4 (64bit)
+: FARO LS 1.1.800.4 (64bit) {FA52D494-A7D5-4C3B-99E1-F8A66A6EEBDC} 8.0.4.5330
+: FARO LS 1.1.700.0 (64bit) {FF6E9382-0B85-48DE-888F-76EFD9A87038} 7.0.0.23
+winget uninstall %param% --purge -e --id {FA52D494-A7D5-4C3B-99E1-F8A66A6EEBDC}
 
 echo %green%Adobe Acrobat Reader DC - Deutsch
 winget  uninstall %param% -e --id "{AC76BA86-7AD7-1031-7B44-AC0F074E4100}"
@@ -65,11 +70,19 @@ echo %purp%Windows Erweiterungen
 echo %purp%==============================
 echo %green%Microsoft Visual C++ 2015-20XX Redistributable 32/64bit (fixworkaround)
 winget  uninstall %param% -e --id "{8bdfe669-9705-4184-9368-db9ce581e0e7}"
+winget  uninstall %param% -e --id "{8bdfe669-9705-4184-9368-db9ce581e0e7}"
 
 
 echo %purp%==============================
 echo %purp%Microsoft
 echo %purp%==============================
+echo %green%Microsoft Access database engine 2010 (English)
+: Microsoft Access database engine 2010 (English) 14.0.7015.1000
+winget  uninstall %param% --purge -e --id  {90140000-00D1-0409-1000-0000000FF1CE}
+echo %green%Microsoft SQL Server 2014 Express LocalDB
+: Microsoft SQL Server 2014 Express LocalDB 12.3.6024.0
+winget  uninstall %param% --purge -e --id {BAF67399-85CD-4555-9B49-1F80EB921C35}
+
 echo %green%Office 365
 asset\office365setup.exe /configure asset\office365remove.xml
 
