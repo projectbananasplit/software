7ab683a9a637734264a26e311a85c02afb71e6ab
Rotzbua <Rotzbua@users.noreply.github.com>
2023-07-07 09:50:16 +0200

    title

--- admin.bat
+++ admin.bat
@@ -1,5 +1,7 @@
 @echo off
 
+TITLE Makerspace Software Updater (Admin)
+
 powershell -noprofile -command "&{ start-process powershell -ArgumentList '-noprofile -ExecutionPolicy Bypass -file .\admin_clean.ps1' -verb RunAs}"
 
 call "admin_clean.bat"
--- user.bat
+++ user.bat
@@ -1,6 +1,6 @@
 @echo off
 
-TITLE Makerspace Software Updater - User
+TITLE Makerspace Software Updater (User)
 call "user_clean.bat"
 call "user_install.bat"
 
