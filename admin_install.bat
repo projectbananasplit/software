b30e73742f42e7e4e1c45f0ce0a6b06ff4c14052
Rotzbua <Rotzbua@users.noreply.github.com>
2023-11-18 11:34:14 +0100

    add param  --disable-interactivity

--- admin_install.bat
+++ admin_install.bat
@@ -1,5 +1,5 @@
 @echo off
-set param=--accept-source-agreements --accept-package-agreements --silent
+set param=--accept-source-agreements --accept-package-agreements --silent --disable-interactivity
 
 @for /f %%a in ('echo prompt $E^| cmd') do set "esc=%%a"
 set red=%esc%[38;5;1m
