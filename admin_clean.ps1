57d87001b1342306a5f0ee41a3dd29eb6a55261e
Rotzbua <Rotzbua@users.noreply.github.com>
2024-01-20 17:07:23 +0100

    Update admin_clean.ps1

--- admin_clean.ps1
+++ admin_clean.ps1
@@ -1,22 +1,19 @@
 ##Remove bloat
 $Bloatware = @(
     #Unnecessary Windows 10/11 AppX Apps
-    "Microsoft.BingFinance"
-    "Microsoft.BingFoodAndDrink"
-    "Microsoft.BingHealthAndFitness"
+    "Microsoft.549981C3F5F10"
     "Microsoft.BingNews"
-    "Microsoft.BingTravel"
-    "Microsoft.BioEnrollment"
-    "Microsoft.DevHome"
     "Microsoft.GamingApp"
     "Microsoft.GetHelp"
     "Microsoft.Getstarted"
     "Microsoft.Messaging"
+    "Microsoft.Microsoft3DViewer"
     "Microsoft.MicrosoftOfficeHub"
     "Microsoft.MicrosoftSolitaireCollection"
     "Microsoft.MixedReality.Portal"
     "Microsoft.NetworkSpeedTest"
     "Microsoft.News"
+    "Microsoft.Office.Desktop"
     "Microsoft.Office.Lens"
     "Microsoft.Office.OneNote"
     "Microsoft.Office.Sway"
@@ -28,6 +25,7 @@ $Bloatware = @(
     "Microsoft.Print3D"
     "Microsoft.RemoteDesktop"
     "Microsoft.SkypeApp"
+    #"Microsoft.StorePurchaseApp"
     "Microsoft.Todos"
     "Microsoft.Wallet"
     "Microsoft.Whiteboard"
@@ -47,13 +45,6 @@ $Bloatware = @(
     "Microsoft.ZuneVideo"
     "MicrosoftTeams"
     "microsoft.windowscommunicationsapps"
-    #"Microsoft.Microsoft3DViewer"
-    #"Microsoft.StorePurchaseApp"
-    #Cortana
-    "Microsoft.549981C3F5F10"
-    #Windows11
-    "*getstarted*"
-    "Microsoft.Windows.ParentalControls"
     #Others
     "SpotifyAB.SpotifyMusic"
     "Disney.37853FC22B2CE"
@@ -79,20 +70,21 @@ $Bloatware = @(
     "clipchamp.clipchamp"
     "*gaming*"
     "MicrosoftCorporationII.MicrosoftFamily"
-    "MicrosoftCorporationII.QuickAssist"
-    #Optional: Typically not removed but you can if you need to for some reason
-    #"*Microsoft.Advertising.Xaml_10.1712.5.0_x64__8wekyb3d8bbwe*"
-    #"*Microsoft.Advertising.Xaml_10.1712.5.0_x86__8wekyb3d8bbwe*"
+    "C27EB4BA.DropboxOEM"
+    "*DevHome*"
+#Optional: Typically not removed but you can if you need to for some reason
+    "*Microsoft.Advertising.Xaml_10.1712.5.0_x64__8wekyb3d8bbwe*"
+    "*Microsoft.Advertising.Xaml_10.1712.5.0_x86__8wekyb3d8bbwe*"
     "*Microsoft.BingWeather*"
-    #"*Microsoft.MSPaint*"
+#"*Microsoft.MSPaint*"
     "*Microsoft.MicrosoftStickyNotes*"
-    #"*Microsoft.Windows.Photos*"
-    #"*Microsoft.WindowsCalculator*"
-    #"*Microsoft.WindowsStore*"
-    "Microsoft.Office.Desktop"
+#"*Microsoft.Windows.Photos*"
+#"*Microsoft.WindowsCalculator*"
+#"*Microsoft.WindowsStore*"
 
 )
-foreach ($Bloat in $Bloatware) {
+foreach ($Bloat in $Bloatware)
+{
     Get-AppxPackage -allusers -Name $Bloat| Remove-AppxPackage -AllUsers
     Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like $Bloat | Remove-AppxProvisionedPackage -Online
     Write-Host "Trying to remove $Bloat."
