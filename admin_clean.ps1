b0763efdea19d0795ae5c6ba92cee098699b2b84
Rotzbua <Rotzbua@users.noreply.github.com>
2024-08-22 16:46:41 +0200

    Update admin_clean.ps1

--- admin_clean.ps1
+++ admin_clean.ps1
@@ -120,6 +120,19 @@ foreach ($sid in $UserSIDs)
 
 Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" BingSearchEnabled -Value 0
 
+##Removes the Microsoft Feeds from displaying
+$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Feeds"
+$Name = "EnableFeeds"
+$value = "0"
+
+if (!(Test-Path $registryPath)) {
+    New-Item -Path $registryPath -Force | Out-Null
+    New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
+}
+
+else {
+    New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
+}
 
 #Stops the Windows Feedback Experience from sending anonymous data
 Write-Host "Stopping the Windows Feedback Experience program"
