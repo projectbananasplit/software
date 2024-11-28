7e670ffbd074e1718d5bfd485ad56226fcbc1911
Rotzbua <Rotzbua@users.noreply.github.com>
2024-11-28 19:14:53 +0100

    Update user_notifications.ps1

--- user_notifications.ps1
+++ user_notifications.ps1
@@ -6,6 +6,9 @@ $targetComputerNames = @("M-CW-MS01", "M-CW-MS02", "M-CW-MS03")
 if ($targetComputerNames -contains $computerName)
 {
     Write-Output "💡 The computer is test pc."
-    schtasks /create /sc DAILY /st 19:45 /tn "PopupOeffnungszeitEnde" /tr "msg * /TIME:300 'Wir schließen in 15 Minuten! Bitte aufräumen!
+    schtasks /delete /tn "PopupOeffnungszeitEnde"
+    schtasks /create /sc DAILY /st 19:45 /tn "PopupOeffnungszeitEnde15Min" /tr "msg * /TIME:300 'Wir schließen in 15 Minuten! Bitte aufräumen!
 We are closing in 15 minutes! Please start cleaning!'" /f
+    schtasks /create /sc DAILY /st 19:55 /tn "PopupOeffnungszeitEnde5Min" /tr "msg * /TIME:300 'Wir schließen in 5 Minuten! Bitte aufräumen!
+We are closing in 5 minutes! Please start cleaning!'" /f
 }
