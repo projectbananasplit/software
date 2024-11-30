08ee5f38b3dfd871521a94c8b7337443d13630c6
Rotzbua <Rotzbua@users.noreply.github.com>
2024-11-30 07:32:32 +0100

    Update user_notifications.ps1

--- user_notifications.ps1
+++ user_notifications.ps1
@@ -1,11 +1,11 @@
-﻿Write-Host "Update Notifications" -ForegroundColor Green -BackgroundColor Gray
+﻿Write-Host "Update Notifications" -ForegroundColor Yellow -BackgroundColor Black
 
 $computerName = $env:COMPUTERNAME
-$targetComputerNames = @("M-CW-MS01", "M-CW-MS02", "M-CW-MS03")
+$targetComputerNames = @("M-CW-MS01", "M-CW-MS02", "M-CW-MS03", "MUC-LAS01", "MUC-LAS02", "MUC-LAS03")
 
 if ($targetComputerNames -contains $computerName)
 {
-    Write-Output "💡 The computer is test pc."
+    Write-Output "💡 The computer is test pc for notifications."
     schtasks /delete /tn "PopupOeffnungszeitEnde" /f
     schtasks /create /sc DAILY /st 19:45 /tn "PopupOeffnungszeitEnde15Min" /tr "msg * /TIME:300 'Wir schließen in 15 Minuten! Bitte aufräumen!
 We are closing in 15 minutes! Please start cleaning!'" /f
