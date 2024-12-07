d2bd59742a530b4ffbd3076fde7fa41508feb978
Rotzbua <Rotzbua@users.noreply.github.com>
2024-12-07 17:50:01 +0100

    f

--- user_notifications.ps1
+++ user_notifications.ps1
@@ -3,25 +3,6 @@
 $computerName = $env:COMPUTERNAME
 $targetComputerNames = @("M-CW-MS01", "M-CW-MS02", "M-CW-MS03", "MUC-LAS01", "MUC-LAS02", "MUC-LAS03", "MUC-LEARN11")
 
-function Set-Wallpaper
-{
-    param (
-        [string]$Path
-    )
-
-    Add-Type -TypeDefinition @"
-    using System;
-    using System.Runtime.InteropServices;
-    public class Wallpaper {
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
-        public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
-    }
-"@
-
-    [Wallpaper]::SystemParametersInfo(0x0014, 0, $Path, 0x0001)
-    Write-Host -ForegroundColor Yellow "Wallpaper updated to $Path"
-}
-
 if ($targetComputerNames -contains $computerName)
 {
     Write-Output "💡 The computer is test pc for notifications."
@@ -30,29 +11,4 @@ if ($targetComputerNames -contains $computerName)
 We are closing in 15 minutes! Please start cleaning!'" /f
     schtasks /create /sc DAILY /st 19:55 /tn "PopupOeffnungszeitEnde5Min" /tr "msg * /TIME:300 'Wir schließen in 5 Minuten! Bitte aufräumen!
 We are closing in 5 minutes! Please start cleaning!'" /f
-
-    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "AppsUseLightTheme" -Value 0
-    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "SystemUsesLightTheme" -Value 0
-    # Write-Host -ForegroundColor Green "Dark theme enabled."
-
-    $WallpaperPath = "$env:USERPROFILE\Pictures\wallpaper.png"
-    $Url = "https://github.com/projectbananasplit/assets/raw/refs/heads/main/wallpaper/ms_default.png"
-
-    Add-Type -TypeDefinition @"
-using System;
-using System.Runtime.InteropServices;
-public class ScreenResolution {
-    [DllImport("user32.dll")]
-    public static extern int GetSystemMetrics(int nIndex);
-}
-"@
-
-    $width = [ScreenResolution]::GetSystemMetrics(0)
-    $height = [ScreenResolution]::GetSystemMetrics(1)
-    Write-Output "Current Resolution: ${width}x${height}"
-
-    Invoke-WebRequest -Uri $Url -OutFile $WallpaperPath
-    Write-Host -ForegroundColor Yellow "Image downloaded to $WallpaperPath"
-
-    Set-Wallpaper -Path $WallpaperPath
 }
---
+++ user_wallpaper.ps1
@@ -0,0 +1,54 @@
+﻿Write-Host "Update Wallpaper" -ForegroundColor Yellow -BackgroundColor Black
+
+$computerName = $env:COMPUTERNAME
+$targetComputerNames = @(
+    "M-CW-MS01", "M-CW-MS02", "M-CW-MS03", "M-CW-MS25",
+    "MUC-LAS01", "MUC-LAS02", "MUC-LAS03", "MUC-LEARN11"
+)
+
+function Set-Wallpaper
+{
+    param (
+        [string]$Path
+    )
+
+    Add-Type -TypeDefinition @"
+    using System;
+    using System.Runtime.InteropServices;
+    public class Wallpaper {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+    }
+"@
+
+    [Wallpaper]::SystemParametersInfo(0x0014, 0, $Path, 0x0001)
+    Write-Host -ForegroundColor Yellow "Wallpaper updated to $Path"
+}
+
+if ($targetComputerNames -contains $computerName)
+{
+    Write-Host "Force dark theme."
+    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "AppsUseLightTheme" -Value 0
+    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "SystemUsesLightTheme" -Value 0
+
+    $WallpaperPath = "$env:USERPROFILE\Pictures\wallpaper.png"
+    $Url = "https://github.com/projectbananasplit/assets/raw/refs/heads/main/wallpaper/ms_default.png"
+
+    Add-Type -TypeDefinition @"
+using System;
+using System.Runtime.InteropServices;
+public class ScreenResolution {
+    [DllImport("user32.dll")]
+    public static extern int GetSystemMetrics(int nIndex);
+}
+"@
+
+    $width = [ScreenResolution]::GetSystemMetrics(0)
+    $height = [ScreenResolution]::GetSystemMetrics(1)
+    Write-Output "Current Resolution: ${width}x${height}"
+
+    Invoke-WebRequest -Uri $Url -OutFile $WallpaperPath
+    Write-Host -ForegroundColor Yellow "Image downloaded to $WallpaperPath"
+
+    Set-Wallpaper -Path $WallpaperPath
+}
