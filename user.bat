e4d314414adcbde1b55dcbad81b616a69f2984ac
Rotzbua <Rotzbua@users.noreply.github.com>
2024-12-07 17:59:46 +0100

    Update user.bat

--- user.bat
+++ user.bat
@@ -7,5 +7,6 @@ call "user_clean.bat"
 call "user_install.bat"
 powershell -ExecutionPolicy Bypass -File user_fusion_plugin.ps1
 powershell -ExecutionPolicy Bypass -File user_notifications.ps1
+powershell -ExecutionPolicy Bypass -File user_wallpaper.ps1
 
 timeout 10
