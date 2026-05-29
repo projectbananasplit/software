ab41c099435881339a1aac4a77c4bd8d4e04cc06
Rotzbua <Rotzbua@users.noreply.github.com>
2026-05-29 19:14:38 +0200

    Update user_clean.bat

--- user_clean.bat
+++ user_clean.bat
@@ -46,7 +46,9 @@ winget uninstall %param% -e --id "9PJPW5LDXLZ5"
 echo %green%Python 3.11
 winget uninstall %param% -e --id "9NRWMJP3717K"
 echo %green%Python 3.12
-winget uninstall %param% -e --id "9NCVDN91XZQP" --source msstore
+::bugged with 3.14
+::winget uninstall %param% -e --id "9NCVDN91XZQP" --source msstore
+::winget uninstall %param% -e --name "Python 3.12" --source msstore
 echo %green%Python 3.13
 winget uninstall %param% -e --id "9PNRBTZXMB4Z"
 echo %green%Outlook for Windows
